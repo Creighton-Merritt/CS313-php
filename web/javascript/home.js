6d5de78481bf0c12b2bee75e0ce746b4b02cb724
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-09-27 15:35:53 -0600

    added pics and javascript

---
+++ web/javascript/home.js
@@ -0,0 +1,20 @@
+
+function waxSeal() {
+    var pic = document.getElementById('imageToChange');
+    pic.src = "images/wax_seal.jpg";
+}
+
+function sailing() {
+    var pic = document.getElementById('imageToChange');
+    pic.src = "images/Before.jpg";
+}
+
+function tipping() {
+    var pic = document.getElementById('imageToChange');
+    pic.src = "images/Capsizing.jpg";
+}
+
+function profile() {
+    var pic = document.getElementById('imageToChange');
+    pic.src = "images/profile_pic.jpg";
+}

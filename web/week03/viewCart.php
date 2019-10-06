6b10d24b85455545eb537406452f975d534f4a45
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-05 18:35:27 -0600

    .

--- web/week03/viewCart.php
+++ web/week03/viewCart.php
@@ -95,7 +95,7 @@ if (!isset($_SESSION["total"]) ) {
                         <td width="15px">&nbsp;</td>
                         <td><?php echo( $_SESSION["prices"][$i]); ?></td>
                         <td width="15px">&nbsp;</td>
-                        <td><a href="?delete=<?php echo($i); ?>">&nbsp;-</a><a href="?add=<?php echo($i); ?>">&nbsp;+</a></td>
+                        <td><a href="?delete=<?php echo($i); ?>"><p>&nbsp;</p>-</a><a href="?add=<?php echo($i); ?>"><p>&nbsp;</p>+</a></td>
                         
                     </tr>
                 <?php

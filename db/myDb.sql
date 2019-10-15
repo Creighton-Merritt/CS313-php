e898924c3962c4621dd0b004377b733bd50a5e2d
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-15 11:46:11 -0600

    .

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,19 +1,19 @@
-CREATE TABLE person {
+CREATE TABLE person (
     person_id serial PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL
-};
+);
 
-CREATE TABLE activity {
+CREATE TABLE activity (
     activity_id serial PRIMARY KEY,
     activity_name VARCHAR(100) NOT NULL
-};
+);
 
-CREATE TABLE items {
+CREATE TABLE items (
 item_id serial PRIMARY KEY,
 item_name VARCHAR(255) NOT NULL
 person_name VARCHAR(50) REFERENCES person(first_name),
 activity_name VARCHAR(100) REFERENCES activity(activity_name)
-};
+);
 
 
 SELECT 

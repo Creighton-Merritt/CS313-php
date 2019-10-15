8c1925fd3f55970d705b0eac1d88445a9db2f8ac
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-15 13:24:35 -0600

    .

--- db/myDb.sql
+++ db/myDb.sql
@@ -9,17 +9,27 @@ CREATE TABLE activity (
 );
 
 CREATE TABLE items (
-item_id serial PRIMARY KEY,
-item_name VARCHAR(255) NOT NULL
-person_name VARCHAR(50) REFERENCES person(first_name),
-activity_name VARCHAR(100) REFERENCES activity(activity_name)
+    item_id serial PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL
+    activity_id VARCHAR(100) REFERENCES activity(activity_id),
+    person_id VARCHAR(50) REFERENCES person(id)
 );
 
 
-SELECT 
-    item_name 
-FROM 
-    items 
-WHERE 
-    person_name = 'name' 
-AND activity_name = 'activity';
+--This seems unecessary
+-- CREATE TABLE item_assign (
+--     item_assign_id serial PRIMARY KEY,
+--     item_name VARCHAR(255) REFERENCES items(item_name),
+--     activity_name VARCHAR(100) REFERENCES activity(activity_name),
+--     person_name VARCHAR(50) REFERENCES person(first_name)
+-- );
+
+
+--join tables to we pull person and item name from those tables based on person and activity id from items.
+-- SELECT 
+--     item_name 
+-- FROM 
+--     items
+-- WHERE 
+--     person_name = 'name' 
+-- AND activity_name = 'activity';

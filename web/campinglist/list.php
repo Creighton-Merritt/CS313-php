7c000ffadf78f5ba54c5f20563b3304628cd3920
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-17 18:00:20 -0600

    .

--- web/campinglist/list.php
+++ web/campinglist/list.php
@@ -38,59 +38,55 @@
 
         </div>
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12"> 
-                    <?php
-                        if ($name == 'Coty') {
-                            $name1 = "1";
-                            $name2 = "8";
-                        } else if ($name == 'Merritt') {
-                            $name1 = "2";
-                            $name2 = "8";
-                        } else if ($name == 'Ethne') {
-                            $name1 = "3";
-                            $name2 = "7";
-                        } else if ($name == 'Indie') {
-                            $name1 = "4";
-                            $name2 = "7";
-                        }
+            <?php
+                if ($name == 'Coty') {
+                    $name1 = "1";
+                    $name2 = "8";
+                } else if ($name == 'Merritt') {
+                    $name1 = "2";
+                    $name2 = "8";
+                } else if ($name == 'Ethne') {
+                    $name1 = "3";
+                    $name2 = "7";
+                } else if ($name == 'Indie') {
+                    $name1 = "4";
+                    $name2 = "7";
+                }
 
-                        if ($location == "Bear Lake") { 
-                            $loc1 = "1"; 
-                        } else if ($location == "Mountains") {
-                            $loc1 = "2"; 
-                        } else if ($location == "San Rafael Swell") {
-                             $loc1 = "3";
-                        }
-                        
-                        $statement = $db->prepare("SELECT item_name, person_name_id, activity_name_id
-                            FROM items
-                            WHERE ((person_name_id = $name1) OR (person_name_id = $name2) or (person_name_id = 5))
-                            AND  ((activity_name_id = $loc1) OR (activity_name_id = 4))
-                            ORDER BY item_name");
-                        $statement->execute();
-                        
-                        echo "<table><tr>";
-                        $count = 0;
-                        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-                        {
+                if ($location == "Bear Lake") { 
+                    $loc1 = "1"; 
+                } else if ($location == "Mountains") {
+                    $loc1 = "2"; 
+                } else if ($location == "San Rafael Swell") {
+                        $loc1 = "3";
+                }
+                
+                $statement = $db->prepare("SELECT item_name, person_name_id, activity_name_id
+                    FROM items
+                    WHERE ((person_name_id = $name1) OR (person_name_id = $name2) or (person_name_id = 5))
+                    AND  ((activity_name_id = $loc1) OR (activity_name_id = 4))
+                    ORDER BY item_name");
+                $statement->execute();
+                
+                echo "<table><tr>";
+                $count = 0;
+                while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                {
+                    
+                    $item_name = $row['item_name'];
+                    {
+                        if ($count < 15) {
+                            echo "<td>$item_name</td>"; 
                             
-                            $item_name = $row['item_name'];
-                            {
-                                if ($count < 15) {
-                                    echo "<td>$item_name</td>"; 
-                                    
-                                    $count++;
-                                } else if ($count == 15) {
-                                    echo "</tr><tr><td>$item_name</td>";
-                                    $count = 0;
-                                }
-                            }
+                            $count++;
+                        } else if ($count == 15) {
+                            echo "</tr><tr><td>$item_name</td>";
+                            $count = 0;
                         }
-                        echo "</tr></table>"
-                    ?> 
-                </div>
-            </div>
+                    }
+                }
+                echo "</tr></table>"
+            ?> 
         </div>
     </body>
 </html>

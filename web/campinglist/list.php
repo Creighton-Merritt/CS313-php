6d6b23ae0f39786cb61d5a07ad57021a3c0025d8
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-18 10:29:18 -0600

    .

--- web/campinglist/list.php
+++ web/campinglist/list.php
@@ -20,8 +20,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
-    <link rel="stylesheet" type="text/css" href="css/home.css">
-    <!-- <script src="javascript/home.js"></script> -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!-- <script src="javascript/main.js"></script> -->
 
 
     <div class="container-fluid">
@@ -35,7 +35,6 @@
         </div>
         <div id="hrline" class="container-fluid">
                 <hr>
-
         </div>
         <div class="container-fluid">
             <?php

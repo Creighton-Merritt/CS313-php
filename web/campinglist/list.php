ddd64990221cd82490ab606d7d27978b8e373568
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-18 17:05:39 -0600

    .

--- web/campinglist/list.php
+++ web/campinglist/list.php
@@ -81,7 +81,7 @@
                     $item_name = $row['item_name'];
                     
                     if ($first == true) {
-                        echo "<td><strong>$location_name</strong></td>";
+                        echo "<td><strong>first, $location_name</strong></td>";
                         $first = false;
                     }
 
@@ -89,21 +89,21 @@
                         if ($previous_location_name_id != $current_location_name_id) {
                             echo "<td class=\"roomHeader\"><strong>$location_name</strong></td>";
                             $previous_location_name_id = $current_location_name_id;
-                            echo "<td>$item_name</td>"; 
+                            echo "<td>if less than, $item_name</td>"; 
                             $count++;
                         } else {
-                            echo "<td>$item_name</td>"; 
+                            echo "<td>else less than,$item_name</td>"; 
                             $count++;
                         } 
                     } 
-                    
+
                     if ($count == 15) {
                         if ($previous_location_name_id != $current_location_name_id) {
                             echo "</tr><tr><td><strong>$location_name</strong></td>";
-                            echo "<td>$item_name</td>";
+                            echo "<td>15 if, $item_name</td>";
                             $count = 0;
                         } else {
-                            echo "</tr><tr><td>$item_name</td>";
+                            echo "</tr><tr><td>15 else $item_name</td>";
                             $count = 0;
                         }
                     }

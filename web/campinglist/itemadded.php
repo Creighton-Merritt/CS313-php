cc28321a81b0b2a28809c97d4feaf88b5e5a0d87
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 17:41:49 -0600

    Fixed

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -20,37 +20,21 @@
     $activity_id = $row['activity_id'];
     $row = $lid->fetch(PDO::FETCH_ASSOC);
     $location_id = $row['location_id'];
-    // $stmt = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
-    //                     VALUES (:newItem, :person_id, :activity_id, :location_id);");
-    // $stmt->bindValue(':newItem', $newItem, PDO::PARAM_STR);
-    // $stmt->bindValue(':person_id', $person_id, PDO::PARAM_INT);
-    // $stmt->bindValue(':activity_id', $activity_id, PDO::PARAM_INT);
-    // $stmt->bindValue(':location_id', $location_id, PDO::PARAM_INT);
-    // $stmt->execute();
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-        <?php
-        echo "<p>$newItem, $person_id, $activity_id, $location_id</p>";
-        ?>
-    </body>
-    </html>
-
-
-    <!-- // This works in postresql, but I can't get it to work with php, so I used the solution above
+    $stmt = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
+                        VALUES (:newItem, :person_id, :activity_id, :location_id);");
+    $stmt->bindValue(':newItem', $newItem, PDO::PARAM_STR);
+    $stmt->bindValue(':person_id', $person_id, PDO::PARAM_INT);
+    $stmt->bindValue(':activity_id', $activity_id, PDO::PARAM_INT);
+    $stmt->bindValue(':location_id', $location_id, PDO::PARAM_INT);
+    $stmt->execute();
+    
+    // // This works in postresql, but I can't get it to work with php, so I used the solution above
     // $statement = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
     // VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
     // (SELECT activity_id from activity where activity_name = '$activity'),
     // (SELECT location_id from location where item_location = '$location'));");
-    // $statement->execute(); -->
-<?php
+    // $statement->execute();
+
 
     // if(isset($_POST["name"])) {
     // header("Location: additems.php");

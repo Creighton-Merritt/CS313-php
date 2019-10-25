2ca36811648e681467957884581f2cdadb040310
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 17:39:48 -0600

    .

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -9,15 +9,17 @@
     $db = get_db();
 
     $pid = $db->prepare("SELECT person_id FROM person WHERE first_name = '$name';");
-    // $aid = $db->prepare("SELECT activity_id FROM activity WHERE activity_name = '$activity';");
-    // $lid = $db->prepare("SELECT location_id FROM location WHERE item_location = '$location';");
+    $aid = $db->prepare("SELECT activity_id FROM activity WHERE activity_name = '$activity';");
+    $lid = $db->prepare("SELECT location_id FROM location WHERE item_location = '$location';");
     $pid->execute();
-    // $aid->execute();
-    // $lid->execute();
+    $aid->execute();
+    $lid->execute();
     $row = $pid->fetch(PDO::FETCH_ASSOC);
     $person_id = $row['person_id'];
-    // $activity_id = $aid->fetch();
-    // $location_id = $lid->fetch();
+    $row = $aid->fetch(PDO::FETCH_ASSOC);
+    $activity_id = $row['activity_id'];
+    $row = $lid->fetch(PDO::FETCH_ASSOC);
+    $location_id = $row['location_id'];
     // $stmt = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
     //                     VALUES (:newItem, :person_id, :activity_id, :location_id);");
     // $stmt->bindValue(':newItem', $newItem, PDO::PARAM_STR);

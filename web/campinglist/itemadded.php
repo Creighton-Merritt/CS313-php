7deb312f7fcc3632d9138171ae0469500dbf8e1b
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 12:49:02 -0600

    .

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -1,33 +1,17 @@
 <?php
-    require ('dbConnect.php');
-    $db = get_db();
-
     $newItem = htmlspecialchars($_POST['newItem']);
     $name = htmlspecialchars($_POST['Name']);
     $activity = htmlspecialchars($_POST['Activity']);
     $location = htmlspecialchars($_POST['Location']);
+
+    $statement = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
+    VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
+    (SELECT activity_id from activity where activity_name = '$activity'),
+    (SELECT location_id from location where location_name = '$item_location'));");
+    $statement->execute();
+    echo "<p>loaded</p>";
+
+    // $new_page = "additems.php";
+    // header("Location: $new_page");
+    // die();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
-    <body>
-        <?php
-            $statement = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
-            VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
-            (SELECT activity_id from activity where activity_name = '$activity'),
-            (SELECT location_id from location where location_name = '$item_location'));");
-            $statement->execute();
-            echo "<p>loaded</p>";
-
-            // $new_page = "additems.php";
-            // header("Location: $new_page");
-            // die();
-        ?>
-
-    </body>
-</html>

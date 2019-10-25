8ab129a711368bf47b3557bb0a1b827a1718230b
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 12:36:25 -0600

    added statement for insert

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -6,9 +6,6 @@
     $name = htmlspecialchars($_POST['Name']);
     $activity = htmlspecialchars($_POST['Activity']);
     $location = htmlspecialchars($_POST['Location']);
-    $name1;
-    $name2;
-    $loc1;
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +16,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
     <body>
-    
+        <?php
+            $statement = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
+            VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
+            (SELECT activity_id from activity where activity_name = '$activity'),
+            (SELECT location_id from location where location_name = '$item_location'));");
+            $statement->execute();
+
+            $new_page = "additems.php";
+            header("Location: $new_page");
+            die();
+        ?>
+
     </body>
 </html>

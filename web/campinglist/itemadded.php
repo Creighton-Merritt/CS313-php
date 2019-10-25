592990d9d64c59e1c305e291fbbaf3fa4bb58df0
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 16:10:34 -0600

    Fixing itemadded.php

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -8,35 +8,32 @@
     require('dbConnect.php');
     $db = get_db();
 
+    $pid = $db->prepare("SELECT person_id FROM person WHERE first_name = '$name';");
+    $aid = $db->prepare("SELECT activity_id FROM activity WHERE activity_name = '$activity';");
+    $lid = $db->prepare("SELECT location_id FROM location WHERE location_name = '$location';");
+    $pid->execute();
+    $aid->execute();
+    $lid->execute();
+    $person_id = $pid->fetch(PDO::FETCH_ASSOC);
+    $activity_id = $aid->fetch(PDO::FETCH_ASSOC);
+    $location_id = $lid->fetch(PDO::FETCH_ASSOC);
+    $stmt = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
+                        VALUES (:newItem, :person_id, :activity_id, :location_id);");
+    $stmt->bindValue(':newItem', $newItem, PDO::PARAM_STR);
+    $stmt->bindValue(':person_id', $person_id, PDO::PARAM_INT);
+    $stmt->bindValue(':activity_id', $activity_id, PDO::PARAM_INT);
+    $stmt->bindValue(':location_id', $location_id, PDO::PARAM_INT);
+    $stmt->execute();
+
+
+    // This works in postresql, but I can't get it to work with php, so I used the solution above
     // $statement = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
     // VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
     // (SELECT activity_id from activity where activity_name = '$activity'),
-    // (SELECT location_id from location where location_name = '$item_location'));");
+    // (SELECT location_id from location where location_name = '$location'));");
     // $statement->execute();
 
-    // $new_page = "additems.php";
-    // header("Location: $new_page");
-    // die();
+    $new_page = "additems.php";
+    header("Location: $new_page");
+    die();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    echo "<p>$newItem</p><br>";
-    echo "<p>$name</p><br>";
-    echo "<p>$activity</p><br>";
-    echo "<p>$location</p><br>";
-    echo "<p>$test</p>";
-    echo "<p>INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
-        VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
-         (SELECT activity_id from activity where activity_name = '$activity'),
-         (SELECT location_id from location where location_name = '$location'))<p>";
-    ?>
-</body>
-</html>

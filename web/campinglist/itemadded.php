8ccd415907bc8590c27931d67ceea1299cf603ab
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 15:45:05 -0600

    .

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -17,9 +17,6 @@
     // $new_page = "additems.php";
     // header("Location: $new_page");
     // die();
-    $statement = $db->prepare("SELECT person_id from person WHERE first_name = '$name';");
-    $statement->execute();
-    $test = $statement['person_id'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,6 +33,10 @@
     echo "<p>$activity</p><br>";
     echo "<p>$location</p><br>";
     echo "<p>$test</p>";
+    echo "<p>INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
+        VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
+         (SELECT activity_id from activity where activity_name = '$activity'),
+         (SELECT location_id from location where location_name = '$item_location'))<p>";
     ?>
 </body>
 </html>

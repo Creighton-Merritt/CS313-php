39be5ad833177fcf6dd462957d119f8e080fb93b
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 16:37:49 -0600

    .

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -7,16 +7,18 @@
 
     require('dbConnect.php');
     $db = get_db();
+    $result = pg_query($db, "SELECT person_id FROM person WHERE first_name = '$name'");
+    $pid = pg_fetch($result);
 
-    $pid = $db->prepare("SELECT person_id FROM person WHERE first_name = '$name';");
-    $aid = $db->prepare("SELECT activity_id FROM activity WHERE activity_name = '$activity';");
-    $lid = $db->prepare("SELECT location_id FROM location WHERE item_location = '$location';");
-    $pid->execute();
-    $aid->execute();
-    $lid->execute();
-    $person_id = $pid->fetch();
-    $activity_id = $aid->fetch();
-    $location_id = $lid->fetch();
+    // $pid = $db->prepare("SELECT person_id FROM person WHERE first_name = '$name';");
+    // $aid = $db->prepare("SELECT activity_id FROM activity WHERE activity_name = '$activity';");
+    // $lid = $db->prepare("SELECT location_id FROM location WHERE item_location = '$location';");
+    // $pid->execute();
+    // $aid->execute();
+    // $lid->execute();
+    // $person_id = $pid->fetch();
+    // $activity_id = $aid->fetch();
+    // $location_id = $lid->fetch();
     // $stmt = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
     //                     VALUES (:newItem, :person_id, :activity_id, :location_id);");
     // $stmt->bindValue(':newItem', $newItem, PDO::PARAM_STR);
@@ -35,18 +37,18 @@
     </head>
     <body>
         <?php
-        echo "<p>$newItem, $person_id, $activity_id, $location_id</p>";
+        echo "<p>$newItem, $pid, $activity_id, $location_id</p>";
         ?>
     </body>
     </html>
 
 
-    // This works in postresql, but I can't get it to work with php, so I used the solution above
+    <!-- // This works in postresql, but I can't get it to work with php, so I used the solution above
     // $statement = $db->prepare("INSERT INTO items(item_name, person_name_id, activity_name_id, location_name_id)
     // VALUES ('$newItem',(SELECT person_id from person where first_name = '$name'),
     // (SELECT activity_id from activity where activity_name = '$activity'),
     // (SELECT location_id from location where item_location = '$location'));");
-    // $statement->execute();
+    // $statement->execute(); -->
 <?php
 
     // if(isset($_POST["name"])) {

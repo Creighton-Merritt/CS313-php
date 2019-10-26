d95ef3c1357de632df307568f3456b7ea5478d81
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 18:20:55 -0600

    .

--- web/campinglist/itemadded.php
+++ web/campinglist/itemadded.php
@@ -41,8 +41,22 @@ if(isset($_POST['submit'])){
         $new_page = "additems.php";
         header("Location: $new_page");
         exit;   
-    } else {
-        echo "<h2>Please select a choice from all fields<h2>";
+    } else { ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Document</title>
+        </head>
+        <body>
+            <?php
+                echo "<h2>Please select a choice from all fields<h2>";
+            ?>
+        </body>
+        </html>
+        <?php
     }
 }
 ?>

92c2f7a3b3ce88f275468d4676716db615f6fdba
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 19:57:48 -0600

    .

--- web/campinglist/additems.php
+++ web/campinglist/additems.php
@@ -39,80 +39,84 @@
         </div>
         <div class="container-fluid">
             <div class = "row justify-content-between, justify-content-around">
-                <h3>Add item to list</h3>
-                <form action="itemadded.php" method="POST">
-                    Item name: 
-                    <input type="text" name="newItem" required><br>
-                    Assign to person: 
-                    <select name="Name" required>
-                        <option value=""></option>
-                        <?php
-                        while ($row = $pstmt->fetch(PDO::FETCH_ASSOC)){
-                            $name = $row['first_name'];
-                            echo "<option value='$name'>$name</option>";
+                <div class="col-md-6"> 
+                    <h3>Add item to list</h3>
+                    <form action="itemadded.php" method="POST">
+                        Item name: 
+                        <input type="text" name="newItem" required><br>
+                        Assign to person: 
+                        <select name="Name" required>
+                            <option value=""></option>
+                            <?php
+                            while ($row = $pstmt->fetch(PDO::FETCH_ASSOC)){
+                                $name = $row['first_name'];
+                                echo "<option value='$name'>$name</option>";
+                            }
+                            ?>
+                        </select><br>       
+                        Assign to camping location: 
+                        <select name="Activity" required>
+                            <option value=""></option>
+                            <?php
+                            while ($row = $astmt->fetch(PDO::FETCH_ASSOC)) {
+                                $activity = $row['activity_name'];
+                                echo "<option value='$activity'>$activity</option>";
+                            }
+                            ?>
+                        </select><br>
+                        Where is it located in the house?: 
+                        <select name="Location" required>
+                            <option value=""></option>
+                            <?php
+                            while ($row = $lstmt->fetch(PDO::FETCH_ASSOC)) {
+                                $location = $row['item_location'];
+                                echo "<option value='$location'>$location</option>";
+                            }
+                            ?>
+                        </select><br>
+                        <input type="submit" value="submit"><br>
+                    </form>  
+                    <?php
+                        if (isset($_GET['success']) && $_GET['success'] == 'true') {
+                            echo "<strong>Item added!<strong><br><br>";
+                            ?>
+                                <meta http-equiv="refresh" content="1;URL=https://hidden-lowlands-67545.herokuapp.com/campinglist/additems.php"/>
+                            <?php
                         }
-                        ?>
-                    </select><br>       
-                    Assign to camping location: 
-                    <select name="Activity" required>
-                        <option value=""></option>
-                        <?php
-                        while ($row = $astmt->fetch(PDO::FETCH_ASSOC)) {
-                            $activity = $row['activity_name'];
-                            echo "<option value='$activity'>$activity</option>";
-                        }
-                        ?>
-                    </select><br>
-                    Where is it located in the house?: 
-                    <select name="Location" required>
-                        <option value=""></option>
-                        <?php
-                        while ($row = $lstmt->fetch(PDO::FETCH_ASSOC)) {
-                            $location = $row['item_location'];
-                            echo "<option value='$location'>$location</option>";
-                        }
-                        ?>
-                    </select><br>
-                    <input type="submit" value="submit"><br>
-                </form>  
-                <?php
-                    if (isset($_GET['success']) && $_GET['success'] == 'true') {
-                        echo "<strong>Item added!<strong><br><br>";
-                        ?>
-                            <meta http-equiv="refresh" content="1;URL=https://hidden-lowlands-67545.herokuapp.com/campinglist/additems.php"/>
-                        <?php
-                    }
-                ?>
+                    ?>
+                </div>
             </div>
             <div class = "row justify-content-between, justify-content-around">
-                <h3>Delete item from list</h3>
-                <form action="" method="POST">
-                        Item name: 
-                        <input type="text" name="d_item_name" required><br>
-                        <input type="submit" value="submit" name="submit_btn"><br>
-                <form>
-                <?php
-                    if(isset($_REQUEST['submit_btn'])) {
-                        $delete_item = $_POST["d_item_name"];
-                    }
-                    $statement = $db->prepare("SELECT i.item_name, p.first_name, a.activity_name
-                                            FROM items i inner join person p on i.person_name_id = p.person_id
-                                            inner join activity a on i.activity_name_id = a.activity_id
-                                            WHERE i.item_name LIKE '$delete_item%';");
-                    $statement->execute();
-                    $count = 0;
-                    echo '<form action="itemdeleted.php" method="POST">';
-                        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-                        {
-                            $ditem = $row['i.item_name'];
-                            $dfirst_name = $row['p.first_name'];
-                            $dactivity = $row['a.activity_name'];
-                            echo '<input type="checkbox" name="' . $count . '" value="' . $ditem . '">' . $ditem . ' - ' . $dfirst_name . ' - ' . $dactivity . '<br>'; 
-                            $count++;
+                <div class="col-md-6"> 
+                    <h3>Delete item from list</h3>
+                    <form action="" method="POST">
+                            Item name: 
+                            <input type="text" name="d_item_name" required><br>
+                            <input type="submit" value="submit" name="submit_btn"><br>
+                    <form>
+                    <?php
+                        if(isset($_REQUEST['submit_btn'])) {
+                            $delete_item = $_POST["d_item_name"];
                         }
-                    echo '<input type="submit" value="submit">';
-                    echo '</form>';
-                ?>
+                        $statement = $db->prepare("SELECT i.item_name, p.first_name, a.activity_name
+                                                FROM items i inner join person p on i.person_name_id = p.person_id
+                                                inner join activity a on i.activity_name_id = a.activity_id
+                                                WHERE i.item_name LIKE '$delete_item%';");
+                        $statement->execute();
+                        $count = 0;
+                        echo '<form action="itemdeleted.php" method="POST">';
+                            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                            {
+                                $ditem = $row['i.item_name'];
+                                $dfirst_name = $row['p.first_name'];
+                                $dactivity = $row['a.activity_name'];
+                                echo '<input type="checkbox" name="' . $count . '" value="' . $ditem . '">' . $ditem . ' - ' . $dfirst_name . ' - ' . $dactivity . '<br>'; 
+                                $count++;
+                            }
+                        echo '<input type="submit" value="submit">';
+                        echo '</form>';
+                    ?>
+                </div>
             </div>
         </div>
     </body>

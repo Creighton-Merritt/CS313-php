29e5ce7c027067188ccce918b80a5ac9b17129c0
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-25 22:25:28 -0600

    Testing update

--- web/campinglist/itemdeleted.php
+++ web/campinglist/itemdeleted.php
@@ -19,6 +19,10 @@
         $new_page = "additems.php?deleted=true";
         header("Location: $new_page");
         exit;   
+    } else if (isset($_POST['edit'])) {
+        echo("You clicked edit");
+
+
     }
 
 ?>

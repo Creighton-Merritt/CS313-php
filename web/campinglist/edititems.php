6f8212caa9973c4ac985dff941e1b013572efbbf
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-26 09:44:19 -0600

    .

--- web/campinglist/edititems.php
+++ web/campinglist/edititems.php
@@ -27,58 +27,57 @@
     </div>
 </head>
 <body>
-        <div class="container-fluid">
-                <a id="home" href="../home.php">&#9668; Home </a>
-                <a id="assignments" href="../assignments.php">CS-313 Assignments &#9658;</a>
-                <br>
-        </div>
-        <div id="hrline" class="container-fluid">
-                <hr>
-        </div>
-        <div class="col-md-auto">
-            <h2>Item lookup</h2>
-                <?php
-                if(!isset($_REQUEST['submit_btn'])) {
-                    ?>
-                    <form action="" method="POST">
-                            Item name: 
-                            <input type="text" name="s_item_name" required><br>
-                            <input type="submit" value="Search" name="submit_btn"><br>
-                    </form>
-                <?php
+    <div class="container-fluid">
+            <a id="home" href="../home.php">&#9668; Home </a>
+            <a id="assignments" href="../assignments.php">CS-313 Assignments &#9658;</a>
+            <br>
+    </div>
+    <div id="hrline" class="container-fluid">
+            <hr>
+    </div>
+    <div class="col-md-auto">
+        <h2>Item lookup</h2>
+        <?php
+        if(!isset($_REQUEST['submit_btn'])) {
+            ?>
+            <form action="" method="POST">
+                    Item name: 
+                    <input type="text" name="s_item_name" required><br>
+                    <input type="submit" value="Search" name="submit_btn"><br>
+            </form>
+        <?php
+        }
+        ?>
+        <?php
+        if(isset($_REQUEST['submit_btn'])) {
+            $select_item = $_POST["s_item_name"];
+            $statement = $db->prepare("SELECT item_name, item_id, first_name, activity_name
+                                    FROM items inner join person on person_name_id = person_id
+                                    inner join activity on activity_name_id = activity_id
+                                    WHERE item_name LIKE '%$select_item%';");
+            $statement->execute();
+            ?>
+            <form action="" method="POST">
+            <?php
+                while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                {
+                    $item_id = $row['item_id'];
+                    $item = $row['item_name'];
+                    $first_name = $row['first_name'];
+                    $activity = $row['activity_name'];
+                    echo "<input type='radio' name='checked[]' value='$item_id'><label>$item - $first_name - $activity</label><br>"; 
                 }
-                ?>
-                <?php
-                if(isset($_REQUEST['submit_btn'])) {
-                    $select_item = $_POST["s_item_name"];
-                    $statement = $db->prepare("SELECT item_name, item_id, first_name, activity_name
-                                            FROM items inner join person on person_name_id = person_id
-                                            inner join activity on activity_name_id = activity_id
-                                            WHERE item_name LIKE '%$select_item%';");
-                    $statement->execute();
-                    ?>
-                    <form action="" method="POST">
-                    <?php
-                        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-                        {
-                            $item_id = $row['item_id'];
-                            $item = $row['item_name'];
-                            $first_name = $row['first_name'];
-                            $activity = $row['activity_name'];
-                            echo "<input type='radio' name='checked[]' value='$item_id'><label>$item - $first_name - $activity</label><br>"; 
-                        }
-                    ?>
-                    <input type="submit" value="Edit item details" name="edit">
-                    </form>
-                <?php
-                }
-                    // //put inside previous if statement?
-                    // if(isset($_REQUEST['edit'])) {
-                    // $get_item = $_POST['checked'];
-                    // $statement = db->prepare
-
-                ?>
-            </div>
-        </div>
+            ?>
+            <input type="submit" value="Edit item details" name="edit">
+            </form>
+        <?php
+        }
+            if(isset($_REQUEST['edit'])) {
+                $get_item = $_POST['checked'];
+                echo ($get_item);
+                // $statement = db->prepare
+            }
+        ?>
+    </div>
 </body>
 </html>

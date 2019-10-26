13d9c6199e98a591f6da26ad083189aef1b38529
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-26 11:35:52 -0600

    .

--- web/campinglist/itemedited.php
+++ web/campinglist/itemedited.php
@@ -5,17 +5,21 @@
     $name = htmlspecialchars($_POST['Name']);
     $activity = htmlspecialchars($_POST['Activity']);
     $location = htmlspecialchars($_POST['Location']);
+    echo($itemname);
+    echo($itemid);
+    echo($name);
+    echo($activity);
+    echo($location);
+    // require('dbConnect.php');
+    // $db = get_db();
 
-    require('dbConnect.php');
-    $db = get_db();
-
-    $statement = $db->prepare("UPDATE items SET item_name = '$itemname',
-                    person_name_id = (SELECT person_id FROM person WHERE first_name = '$name'),
-                    activity_name_id = (SELECT activity_id FROM activity WHERE activity_name = '$activity'),
-                    location_name_id = (SELECT location_id FROM location WHERE item_location = '$location')
-                    WHERE item_id = $itemid;");
-    $statement->execute();
-    $new_page = "edititems.php?edit=true";
-    header("Location: $new_page");
-    exit;   
+    // $statement = $db->prepare("UPDATE items SET item_name = '$itemname',
+    //                 person_name_id = (SELECT person_id FROM person WHERE first_name = '$name'),
+    //                 activity_name_id = (SELECT activity_id FROM activity WHERE activity_name = '$activity'),
+    //                 location_name_id = (SELECT location_id FROM location WHERE item_location = '$location')
+    //                 WHERE item_id = $itemid;");
+    // $statement->execute();
+    // $new_page = "edititems.php?edit=true";
+    // header("Location: $new_page");
+    // exit;   
 ?>

e156385849f2024b67a5045959a84f63892e1fbd
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-29 11:46:48 -0600

    Fixing bugs

--- web/week07/signIn.php
+++ web/week07/signIn.php
@@ -8,7 +8,7 @@ if (isset($_POST['username']) && isset($_POST['userpass'])) {
 	require("dbConnect.php");
 	$db = get_db();
 
-	$stmt = $db->prepare("SELECT user_password FROM login WHERE username = :username";);
+	$stmt = $db->prepare("SELECT user_password FROM login WHERE username = :username");
 	$stmt->bindValue(':username', $usernam, PDO::PARAM_STR);
 	$stmt->execute();
 
@@ -23,39 +23,33 @@ if (isset($_POST['username']) && isset($_POST['userpass'])) {
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['username'] = $username;
 	}
-	
-	
-
+}
 
 ?>
 
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Sign In</title>
 </head>
-
 <body>
-<div>
+	<h1>Sign in</h1>
 
-<h1>Sign in</h1>
+	<form action="signIn.php" method="POST">
 
-<form action="signIn.php" method="POST">
+		Username:
+		<input type="text" id="username" name="username" placeholder="Username" required><br><br>
 
-    Username:
-	<input type="text" id="username" name="username" placeholder="Username" required><br><br>
+		Password:
+		<input type="password" id="userpass" name="userpass" placeholder="Password" required><br><br>
 
-    Password:
-	<input type="password" id="userpass" name="userpass" placeholder="Password" required><br><br>
+		<input type="submit" value="Sign In" />
 
-	<input type="submit" value="Sign In" />
-
-</form>
-<br><br>
-No Account? <a href="signUp.php">Sign up here.</a><br><br>
-
-</div>
-
+	</form>
+	<br><br>
+	No Account? <a href="signUp.php">Sign up here.</a><br><br>
 </body>
 </html>

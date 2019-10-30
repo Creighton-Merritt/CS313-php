30a1958bcead7e72f95a91045138e272d674ab64
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-30 10:50:54 -0600

    .

---
+++ web/campinglist/test/additems.php
@@ -0,0 +1,79 @@
+<?php
+    // Queries for drop down lists below
+    require ('dbConnect.php');
+    $db = get_db();
+    $pstmt = $db->prepare("SELECT first_name from person;");
+    $pstmt->execute();
+    $lstmt = $db->prepare("SELECT item_location from location;");
+    $lstmt->execute();
+    $astmt = $db->prepare("SELECT activity_name from activity ORDER BY activity_id;");
+    $astmt->execute();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
+    <link rel="stylesheet" type="text/css" href="../css/main.css>
+
+        <div class = "container-fluid">
+            <div class ="row">
+                <div class="col-md-6"> 
+                    <h2>Add item to list</h2>
+                    <!-- Form for adding items. I run a query for each drop down list category and pass the value to itemadded.php -->
+                    <form action="itemadded.php" method="POST">
+                        Item name: 
+                        <input type="text" name="newItem" required><br>
+                        Who will use it?: 
+                        <select name="Name" required>
+                            <option value=""></option>
+                            <?php
+                            while ($row = $pstmt->fetch(PDO::FETCH_ASSOC)){
+                                $name = $row['first_name'];
+                                echo "<option value='$name'>$name</option>";
+                            }
+                            ?>
+                        </select><br>       
+                        Camping location: 
+                        <select name="Activity" required>
+                            <option value=""></option>
+                            <?php
+                            while ($row = $astmt->fetch(PDO::FETCH_ASSOC)) {
+                                $activity = $row['activity_name'];
+                                echo "<option value='$activity'>$activity</option>";
+                            }
+                            ?>
+                        </select><br>
+                        Where is it located in the house?: 
+                        <select name="Location" required>
+                            <option value=""></option>
+                            <?php
+                            while ($row = $lstmt->fetch(PDO::FETCH_ASSOC)) {
+                                $location = $row['item_location'];
+                                echo "<option value='$location'>$location</option>";
+                            }
+                            ?>
+                        </select><br>
+                        <input type="submit" value="Submit"><br>
+                    </form>  
+                    <?php
+                        // Code for confirmation message. If item was added it pops up for 1-2 seconds before the page refreshes.
+                        if (isset($_GET['success']) && $_GET['success'] == 'true') {
+                            echo '<p><strong>Item added!</strong></p>';
+                            ?>
+                                <meta http-equiv="refresh" content="1;URL=https://hidden-lowlands-67545.herokuapp.com/campinglist/additems.php"/>
+                            <?php
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ web/campinglist/test/list.php
@@ -0,0 +1,110 @@
+<?php
+    require "dbConnect.php";
+    $db = get_db();
+
+    $name = htmlspecialchars($_POST['Name']);
+    $location = htmlspecialchars($_POST['Location']);
+    $name1;
+    $name2;
+    $loc1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <div class="container-fluid">
+        <h1><?php echo $name?> 's <?php echo $location ?> Packing List</h1>
+    </div>
+</head>
+    <body>
+        <div class="container-fluid">
+                <a id="home" href="main.html">&#9668; Back </a>
+                <a id="assignments" href="../assignments.php">CS-313 Assignments &#9658;</a>
+                <br>
+        </div>
+        <div id="hrline" class="container-fluid">
+                <hr>
+        </div>
+        <div class="container-fluid">
+            <?php
+                if ($name == 'Coty') {
+                    $name1 = "1";
+                    $name2 = "8";
+                } else if ($name == 'Merritt') {
+                    $name1 = "2";
+                    $name2 = "8";
+                } else if ($name == 'Ethne') {
+                    $name1 = "3";
+                    $name2 = "7";
+                } else if ($name == 'Indie') {
+                    $name1 = "4";
+                    $name2 = "7";
+                }
+
+                if ($location == "Bear Lake") { 
+                    $loc1 = "1"; 
+                } else if ($location == "Uinta Mountains") {
+                    $loc1 = "2"; 
+                } else if ($location == "San Rafael Swell") {
+                        $loc1 = "3";
+                }
+                
+                $statement = $db->prepare("SELECT item_name, person_name_id, activity_name_id, location_name_id, item_location
+                        FROM location
+                        LEFT JOIN items
+                        ON (location_id = location_name_id)
+                        WHERE ((person_name_id = $name1) OR (person_name_id = $name2) or (person_name_id = 5))
+                        AND  ((activity_name_id = $loc1) OR (activity_name_id = 4))
+                        ORDER BY location_name_id;");
+                $statement->execute();
+                echo "<table><tr>";
+                $count = 0;
+                $first = true;
+                $previous_location_name_id = 1;
+                
+                //List items by location in house to make packing more efficient. 
+                while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                {
+                    $current_location_name_id = $row['location_name_id'];
+                    $location_name = $row['item_location'];
+                    $item_name = $row['item_name'];
+                    
+                    if ($first == true) {
+                        echo "<td><strong>$location_name</strong></td>";
+                        $first = false;
+                    }
+
+                    if ($count == 15) {
+                        if ($previous_location_name_id != $current_location_name_id) {
+                            echo "</tr><tr><td><strong>$location_name</strong></td>";
+                            $previous_location_name_id = $current_location_name_id;
+                        } else {
+                            echo "</tr><tr>";
+                        }
+                        echo "<td>$item_name</td>";
+                        $count = 0;
+                    } else {
+                        if ($previous_location_name_id != $current_location_name_id) {
+                            echo "<td class=\"roomHeader\"><strong>$location_name</strong></td>";
+                            $previous_location_name_id = $current_location_name_id;  
+                        } 
+                        echo "<td>$item_name</td>"; 
+                        $count++;
+                    }
+                    
+                }  
+                echo "</tr></table>"
+            ?> 
+        </div>
+    </body>
+</html>

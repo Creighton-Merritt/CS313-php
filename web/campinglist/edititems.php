9d8d4bbd51c86f07e9d680b920852e189207e1ca
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-10-31 13:58:45 -0600

    Fixing bugs

--- web/campinglist/edititems.php
+++ web/campinglist/edititems.php
@@ -85,8 +85,9 @@
                     <form action="itemedited.php" method="POST">
                         <br><br>
                         <h3>Edit item details</h3><br>
+                        Item name:
                         <?php        
-                        echo"Item name: <input type='text' name='itemid' value='$itemid' id='hidden'><br>";
+                        echo"<input type='text' name='itemid' value='$itemid' id='hidden'><br>";
                         echo"<input type='text' name='itemname' value='$itemname'required><br>";
                     }
                         ?>
